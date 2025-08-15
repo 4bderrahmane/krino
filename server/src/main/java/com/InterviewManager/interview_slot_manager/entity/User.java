b0645f9d4a8a2e89381e1cc79555e59f960df308
4bderrahmane <4bderrahmane@gmail.com>
2025-08-15 17:44:27 +0100

    I added the AdminInitializer Class to create the admin account with the ADMIN_ROLE

--- server/src/main/java/com/InterviewManager/interview_slot_manager/entity/User.java
+++ server/src/main/java/com/InterviewManager/interview_slot_manager/entity/User.java
@@ -1,10 +1,7 @@
 package com.InterviewManager.interview_slot_manager.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
@@ -12,8 +9,8 @@ import java.time.LocalDateTime;
 import java.util.Set;
 import java.util.HashSet;
 
-@Getter
-@Setter
+@Builder
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
