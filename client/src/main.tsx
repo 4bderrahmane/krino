35893cd706857e883f5754b523c81967e7d87816
4bderrahmane <4bderrahmane@gmail.com>
2025-08-18 02:03:42 +0100

    I updated the main.tsx file to include the BrowserRouter tag!

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,15 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import './utils/i18n'
-import LoadingSpinner from "./shared/components/LoadingSpinner.tsx";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-
-      <LoadingSpinner>
-      </LoadingSpinner>
-  </StrictMode>,
-)
+    <React.StrictMode>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </React.StrictMode>
+);
