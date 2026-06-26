72905f63d97908e84e617fbe73656ccf5239879e
4bderrahmane <me@4bderrahmane.com>
2026-06-26 18:08:02 +0100

    minio client Bean with StorageProperties config

---
+++ server/src/main/java/com/krino/backend/configuration/StorageConfiguration.java
@@ -0,0 +1,19 @@
+package com.krino.backend.configuration;
+
+import io.minio.MinioClient;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties(StorageProperties.class)
+public class StorageConfiguration {
+
+    @Bean
+    public MinioClient minioClient(StorageProperties properties) {
+        return MinioClient.builder()
+                .endpoint(properties.getEndpoint())
+                .credentials(properties.getAccessKey(), properties.getSecretKey())
+                .build();
+    }
+}
