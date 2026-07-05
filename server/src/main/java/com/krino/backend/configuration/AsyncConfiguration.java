ea70d76fe4b474cf7e868a11064f7d8035c69c31
4bderrahmane <me@4bderrahmane.com>
2026-07-05 02:50:39 +0100

    Async config file

---
+++ server/src/main/java/com/krino/backend/configuration/AsyncConfiguration.java
@@ -0,0 +1,21 @@
+package com.krino.backend.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@Configuration
+@EnableAsync
+class AsyncConfiguration {
+
+    @Bean("mailExecutor")
+    ThreadPoolTaskExecutor mailExecutor() {
+        var ex = new ThreadPoolTaskExecutor();
+        ex.setCorePoolSize(2);
+        ex.setMaxPoolSize(4);
+        ex.setQueueCapacity(50);
+        ex.setThreadNamePrefix("mail-");
+        return ex;
+    }
+}
